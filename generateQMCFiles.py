ec4c7c3759d6b36558c0ab4466b6e30f40eee98a
madgal <galbraithm001@gmail.com>
2017-07-06 15:23:36 -0500

    Update generate file to modify *.xml files by using lxml for simpler code

--- generateQMCFiles.py
+++ generateQMCFiles.py
@@ -1,168 +1,140 @@
 def generateCuspCorrection(directory,fileroot,sub_path,baseName):
     import os
 
-    ##################################################################
-    ### Cusp.xml
-    ##################################################################
-    main = "<?xml version=\"1.0\"?>\n"
-    main = main + "<simulation>\n"
-    main = main +"  <project id=\""+fileroot+"\" series=\"1\"/>\n"
-    main = main +"  <!-- input from quantum package converter -->\n"
-    main = main +"  <include href=\""+sub_path+"/"+baseName+"/"+fileroot+".ptcl.xml\"/>\n"
-    main = main +"  <include href=\""+sub_path+"/"+baseName+"/"+fileroot+".wfs.xml\"/>\n"
-    main = main +"  <!--  Hamiltonian -->\n"
-    main = main +"  <hamiltonian name=\"h0\" type=\"generic\" target=\"e\">\n"
-    main = main +"    <pairpot name=\"IonElec\" type=\"coulomb\" source=\"ion0\" target=\"e\"/>\n"
-    main = main +"    <constant name=\"IonIon\" type=\"coulomb\" source=\"ion0\" target=\"ion0\"/>\n"
-    main = main +"    <pairpot name=\"ElecElec\" type=\"coulomb\" source=\"e\" target=\"e\" physical=\"true\"/>\n"
-    main = main +"  </hamiltonian>\n\n\n"
-    main = main +" <init source=\"ion0\" target=\"e\"/>\n\n\n\n"
-    main = main +"  <qmc method=\"vmc\" move=\"pbyp\" gpu=\"yes\">\n"
-    main = main +"    <estimator name=\"LocalEnergy\" hdf5=\"no\"/>\n"
-    main = main +"    <parameter name=\"walkers\">   1</parameter>\n"
-    main = main +"    <parameter name=\"samplesperthread\">    1 </parameter>\n"
-    main = main +"    <parameter name=\"stepsbetweensamples\">    10 </parameter>\n"
-    main = main +"    <parameter name=\"substeps\">  30 </parameter>\n"
-    main = main +"    <parameter name=\"warmupSteps\">  25 </parameter>\n"
-    main = main +"    <parameter name=\"blocks\"> 10</parameter>\n"
-    main = main +"    <parameter name=\"timestep\">  0.001 </parameter>\n"
-    main = main +"    <parameter name=\"usedrift\">   no </parameter>\n"
-    main = main +"  </qmc>\n\n"
-    main = main +"</simulation>\n"
+    ################################################
+    #### Generate : Cusp.xml
+    ################################################
+    import lxml
+    from lxml import etree
 
-    thisfile = directory +"/CuspCorrection/Cusp.xml"
-    with open(thisfile,"w") as fileOut:
-	fileOut.write("%s" %main)
+    os.system("cp misc/Cusp.xml " +directory +"/CuspCorrection/")
+
+    myFile = directory+"/CuspCorrection/Cusp.xml"
+    tree = etree.parse(myFile)
+    ## Modify Cusp.xml for your system
+
+    root = tree.getroot()
+
+    project = root[0]
+    icld_ptcl = root[2]
+    icld_wfs = root[3]
+    
+    project.set("id",fileroot)
+
+    ptclFile = sub_path+"/"+baseName+"/"+fileroot+".ptcl.xml"
+    wfsFile =  sub_path+"/"+baseName+"/"+fileroot+".wfs.xml"
+    icld_ptcl.set("href",ptclFile)
+    icld_wfs.set("href",wfsFile)
+
+    ###### NOW WRITE THE MODIFICATIONS TO A FILE
+    tmpfile = myfile+".tmp"
+    f = open( tmpfile,"w")
+    f.write("<?xml version=\"1.0\"?>\n")
+    f.write(etree.tostring(root,pretty_print=True))
+    f.close()
+
+    os.system("mv " + tmpfile + " " + myfile)
 
 
-    #### cusp.sh
+    ################################################
+    #### Generate : cusp.sh
+    ################################################
     main = "#!/bin/bash\n\n"
 
     main = main +"BINDIR=/soft/applications/qmcpack/github/build_intel_real/bin\n"
-    main = main +"FILEIN="+sub_path+"/"+baseName+"/CuspCorrection/cusp.xml\n"
+    main = main +"FILEIN="+sub_path+"/"+baseName+"/CuspCorrection/Cusp.xml\n"
     main = main +"$BINDIR/qmcpack $FILEIN \n"
 
     thisfile = directory +"/CuspCorrection/cusp.sh"
     with open(thisfile,"w") as fileOut:
 	fileOut.write("%s" %main)
 
+    ################################################
+    #### Generate : cusp_submission.sh
+    ################################################
     os.system("cp misc/cusp_submission.sh "+directory+"/CuspCorrection/")
 
 
 def generateOptimization(directory,fileroot,sub_path,baseName):
     import os
+    ################################################
+    #### Generate : Opt.xml
+    ################################################
+    import lxml
+    from lxml import etree
 
-    main = "<?xml version=\"1.0\"?>\n"
-    main = main + "<simulation>\n"
-    main = main +"  <project id=\"Opt-"+fileroot+"\" series=\"1\"/>\n"
-    main = main +"  <!-- input from quantum package converter -->\n"
-    main = main +"  <include href=\""+sub_path+"/"+baseName+"/"+fileroot+".ptcl.xml\"/>\n"
-    main = main +"  <include href=\""+sub_path+"/"+baseName+"/"+fileroot+".wfs.xml\"/>\n"
-    main = main +"  <!--  Hamiltonian -->\n"
-    main = main +"  <hamiltonian name=\"h0\" type=\"generic\" target=\"e\">\n"
-    main = main +"    <pairpot name=\"IonElec\" type=\"coulomb\" source=\"ion0\" target=\"e\"/>\n"
-    main = main +"    <constant name=\"IonIon\" type=\"coulomb\" source=\"ion0\" target=\"ion0\"/>\n"
-    main = main +"    <pairpot name=\"ElecElec\" type=\"coulomb\" source=\"e\" target=\"e\" physical=\"true\"/>\n"
-    main = main +"  </hamiltonian>\n\n\n"
-    main = main +" <init source=\"ion0\" target=\"e\"/>\n"
-    main = main +"  <qmc method=\"vmc\" move=\"pbyp\" checkpoint=\"-1\" gpu=\"yes\">\n"
-    main = main +"    <estimator name=\"LocalEnergy\" hdf5=\"no\"/>\n"
-    main = main +"    <parameter name=\"walkers\">   1</parameter>\n"
-    main = main +"    <parameter name=\"step\">    1</parameter>\n"
-    main = main +"    <parameter name=\"substeps\">  5 </parameter>\n"
-    main = main +"    <parameter name=\"warmupSteps\">  10 </parameter>\n"
-    main = main +"    <parameter name=\"blocks\"> 50</parameter>\n"
-    main = main +"    <parameter name=\"timestep\">  0.5 </parameter>\n"
-    main = main +"    <parameter name=\"usedrift\">   no </parameter>\n"
-    main = main +"  </qmc>\n\n"
+    os.system("cp misc/Opt.xml " +directory +"/Optimization/")
 
-    main = main +"<loop max=\"2\">\n"
-    main = main +"  <qmc method=\"linear\" move=\"pbyp\" checkpoint=\"-1\" gpu=\"yes\">\n"
-    main = main +"    <parameter name=\"blocks\"> 20</parameter>\n"
-    main = main +"    <parameter name=\"warmupSteps\">  40</parameter>\n"
-    main = main +"    <parameter name=\"substeps\">  20 </parameter>\n"
-    main = main +"    <parameter name=\"timestep\">  0.5 </parameter>\n"
-    main = main +"    <parameter name=\"walkers\">   1</parameter>\n"
-    main = main +"    <parameter name=\"minwalkers\">  0.001 </parameter>\n"
-    main = main +"    <parameter name=\"usedrift\">   no </parameter>\n"
-    main = main +"    <estimator name=\"LocalEnergy\" hdf5=\"no\"/>\n"
-    main = main +"    <cost name=\"energy\">                   0.9 </cost>\n"
-    main = main +"    <cost name=\"unreweightedvariance\">     0.0 </cost>\n"
-    main = main +"    <cost name=\"reweightedvariance\">       0.1 </cost>\n"
-    main = main +"    <parameter name=\"MinMethod\">OneShiftOnly</parameter>\n"
-    main = main +"    <parameter name=\"nonlocalpp\">yes</parameter>\n"
-    main = main +"    <parameter name=\"useBuffer\">no</parameter>\n"
-    main = main +"  </qmc>\n"
-    main = main +"</loop>\n"
+    myFile = directory+"/Optimization/Opt.xml"
+    tree = etree.parse(myFile)
+    ## Modify Cusp.xml for your system
 
-    main = main +"<loop max=\"40\">\n"
-    main = main +"  <qmc method=\"linear\" move=\"pbyp\" checkpoint=\"-1\" gpu=\"yes\">\n"
-    main = main +"    <parameter name=\"blocks\"> 40</parameter>\n"
-    main = main +"    <parameter name=\"warmupSteps\">  40</parameter>\n"
-    main = main +"    <parameter name=\"substeps\">  20 </parameter>\n"
-    main = main +"    <parameter name=\"timestep\">  0.5 </parameter>\n"
-    main = main +"    <parameter name=\"walkers\">   1</parameter>\n"
-    main = main +"    <parameter name=\"minwalkers\">  0.5 </parameter>\n"
-    main = main +"    <parameter name=\"usedrift\">   no </parameter>\n"
-    main = main +"    <estimator name=\"LocalEnergy\" hdf5=\"no\"/>\n"
-    main = main +"    <cost name=\"energy\">                   0.9 </cost>\n"
-    main = main +"    <cost name=\"unreweightedvariance\">     0.0 </cost>\n"
-    main = main +"    <cost name=\"reweightedvariance\">       0.1 </cost>\n"
-    main = main +"    <parameter name=\"MinMethod\">OneShiftOnly</parameter>\n"
-    main = main +"    <parameter name=\"nonlocalpp\">yes</parameter>\n"
-    main = main +"    <parameter name=\"useBuffer\">no</parameter>\n"
-    main = main +"    <parameter name=\"shift_i\"> 0.1</parameter>\n"
-    main = main +"  </qmc>\n"
-    main = main +"</loop>\n"
+    root = tree.getroot()
 
-    main = main +"</simulation>\n"
-    thisfile = directory +"/Optimization/Opt.xml"
-    with open(thisfile,"w") as fileOut:
-	fileOut.write("%s" %main)
+    project = root[0]
+    icld_ptcl = root[2]
+    icld_wfs = root[3]
+    
+    projName = "Opt-"+fileroot
+    project.set("id",projName)
 
+    ptclFile = sub_path+"/"+baseName+"/"+fileroot+".ptcl.xml"
+    wfsFile =  sub_path+"/"+baseName+"/"+fileroot+".wfs.xml"
+    icld_ptcl.set("href",ptclFile)
+    icld_wfs.set("href",wfsFile)
+
+    ###### NOW WRITE THE MODIFICATIONS TO A FILE
+    tmpfile = myfile+".tmp"
+    f = open( tmpfile,"w")
+    f.write("<?xml version=\"1.0\"?>\n")
+    f.write(etree.tostring(root,pretty_print=True))
+    f.close()
+
+    os.system("mv " + tmpfile + " " + myfile)
+
+    ################################################
+    ### Generate bgq-Opt.sh
+    ################################################
     os.system("cp misc/bgq-Opt.sh "+directory+"/Optimization/")
 
 def generateDMC(directory,fileroot,sub_path,baseName):
     import os
+    ################################################
+    #### Generate : Opt.xml
+    ################################################
+    import lxml
+    from lxml import etree
 
+    os.system("cp misc/DMC.xml " +directory +"/DMC/")
 
-    main = "<?xml version=\"1.0\"?>\n"
-    main = main + "<simulation>\n"
-    main = main +"  <project id=\"DMC-"+fileroot+"\" series=\"1\"/>\n"
-    main = main +"  <!-- input from quantum package converter -->\n"
-    main = main +"  <include href=\""+sub_path+"/"+baseName+"/"+fileroot+".ptcl.xml\"/>\n"
-    main = main +"  <include href=\""+sub_path+"/"+baseName+"/"+fileroot+".wfs.xml\"/>\n"
-    main = main +"  <!--  Hamiltonian -->\n"
-    main = main +"  <hamiltonian name=\"h0\" type=\"generic\" target=\"e\">\n"
-    main = main +"    <pairpot name=\"IonElec\" type=\"coulomb\" source=\"ion0\" target=\"e\"/>\n"
-    main = main +"    <constant name=\"IonIon\" type=\"coulomb\" source=\"ion0\" target=\"ion0\"/>\n"
-    main = main +"    <pairpot name=\"ElecElec\" type=\"coulomb\" source=\"e\" target=\"e\" physical=\"true\"/>\n"
-    main = main +"  </hamiltonian>\n\n\n"
-    main = main +" <init source=\"ion0\" target=\"e\"/>\n"
-    main = main +"  <qmc method=\"vmc\" move=\"pbyp\" gpu=\"yes\">\n"
-    main = main +"    <estimator name=\"LocalEnergy\" hdf5=\"no\"/>\n"
-    main = main +"    <parameter name=\"walkers\">   1</parameter>\n"
-    main = main +"    <parameter name=\"samplesperthread\">    1 </parameter>\n"
-    main = main +"    <parameter name=\"stepsbetweensamples\">    10 </parameter>\n"
-    main = main +"    <parameter name=\"substeps\">  30 </parameter>\n"
-    main = main +"    <parameter name=\"warmupSteps\"> 100 </parameter>\n"
-    main = main +"    <parameter name=\"blocks\"> 10</parameter>\n"
-    main = main +"    <parameter name=\"timestep\">  0.5 </parameter>\n"
-    main = main +"    <parameter name=\"usedrift\">   no </parameter>\n"
-    main = main +"  </qmc>\n\n\n"
-    main = main +"  <qmc method=\"dmc\" move=\"pbyp\" checkpoint=\"-1\" gpu=\"yes\">\n"
-    main = main +"    <estimator name=\"LocalEnergy\" hdf5=\"no\"/>\n"
-    main = main +"    <parameter name=\"targetwalkers\">4000</parameter>\n"
-    main = main +"    <parameter name=\"reconfiguration\">   no </parameter>\n"
-    main = main +"    <parameter name=\"warmupSteps\">  50 </parameter>\n"
-    main = main +"    <parameter name=\"timestep\">  0.001 </parameter>\n"
-    main = main +"    <parameter name=\"steps\">   30 </parameter>\n"
-    main = main +"    <parameter name=\"blocks\">  100</parameter>\n"
-    main = main +"    <parameter name=\"nonlocalmoves\">  yes </parameter>\n"
-    main = main +"  </qmc>\n"
-    main = main +"</simulation>\n"
-    thisfile = directory +"/DMC/DMC.xml"
-    with open(thisfile,"w") as fileOut:
-	fileOut.write("%s" %main)
+    myFile = directory+"/DMC/DMC.xml"
+    tree = etree.parse(myFile)
+    ## Modify Cusp.xml for your system
 
+    root = tree.getroot()
+
+    project = root[0]
+    icld_ptcl = root[2]
+    icld_wfs = root[3]
+    
+    projName = "DMC-"+fileroot
+    project.set("id",projName)
+
+    ptclFile = sub_path+"/"+baseName+"/"+fileroot+".ptcl.xml"
+    wfsFile =  sub_path+"/"+baseName+"/"+fileroot+".wfs.xml"
+    icld_ptcl.set("href",ptclFile)
+    icld_wfs.set("href",wfsFile)
+
+    ###### NOW WRITE THE MODIFICATIONS TO A FILE
+    tmpfile = myfile+".tmp"
+    f = open( tmpfile,"w")
+    f.write("<?xml version=\"1.0\"?>\n")
+    f.write(etree.tostring(root,pretty_print=True))
+    f.close()
+
+    os.system("mv " + tmpfile + " " + myfile)
+
+    ################################################
+    ### Generate bgq-DMC.sh
+    ################################################
     os.system("cp misc/bgq-DMC.sh "+directory+"/DMC/")

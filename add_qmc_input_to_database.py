5e2a1fbd643f8df7970caa6a8b9b44ce90a28693
madgal <galbraithm001@gmail.com>
2017-07-10 14:46:36 -0500

    Added methods to pull QMC run information from input files

--- add_qmc_input_to_database.py
+++ add_qmc_input_to_database.py
@@ -6,7 +6,7 @@ You can add_input
 
 Usage:
   add_qmc_input_to_database.py (-h | --help)
-  add_qmc_input_to_database.py add_input --path=<path>
+  add_qmc_input_to_database.py add_input --path=<path> --wfs_flnm=<name_of_wavefunction_xml_file> --ptcl_flnm=<name_of_particle_xml_file> --opt_flnm=<name_of_optimization_xml_file> --dmc_flnm=<name_of_dmc_xml_file>
                      (--method=<method_name> --basis=<basis_name>
                       --geo=<geometry_name> --comment=<comment>|
                       --run_id=<id>)
@@ -71,8 +71,13 @@ if __name__ == '__main__':
 		Jastrow = (true | false)
 		CuspCorrection = (true | false)
 		Version = (version number)
+                vmc_preOpt
 		Optimization_Block =???
+                vmc_preDMC
 		DMC_block = ???
+                cutoff
+                n_det
+                pseudopotential
 		basis
 		geometry
 		comments
@@ -83,6 +88,116 @@ if __name__ == '__main__':
 		
 	
 		'''
+
+          '''
+         <simulation>
+
+          ...
+
+          <qmc method="dmc" move="pbyp" checkpoint="-1" gpu="yes">
+            <estimator name="LocalEnergy" hdf5="no"/>
+            <parameter name="targetwalkers">4000</parameter>
+            <parameter name="reconfiguration">   no </parameter>
+            <parameter name="warmupSteps">  50 </parameter>
+            <parameter name="timestep">  0.001 </parameter>
+            <parameter name="steps">   30 </parameter>
+            <parameter name="blocks">  200</parameter>
+            <parameter name="nonlocalmoves">  yes </parameter>
+          </qmc>
+         </simulation>
+         
+        -----------
+        The following commands print the results beneath:
+          root.tag 
+             > 'simulation'
+          root[8].tag 
+	     > 'qmc'
+          root[8].attrib
+	     > {'checkpoint': '-1', 'gpu': 'yes', 'move': 'pbyp', 'method': 'dmc'}
+
+
+          tags = ""
+          for x in root[8].attrib:
+               tags = tags +x +'=' +root[8].attrib[x] +","
+          print tags[:-1]
+
+             >'method=dmc,move=pbyp,checkpoint=-1,gpu=yes'
+
+          root[8][1].attrib
+	     > {'name': 'targetwalkers'}
+          root[8][1].text
+             > '4000'
+          root[8][1].attrib['name']
+             > 'targetwalkers'
+          root[8][1].attrib['name'] + '='
+             > 'targetwalkers='
+          root[8][1].attrib['name'] + '=' + root[8][1].text
+             > 'targetwalkers=4000'
+
+
+          params = ""
+          for child in root[8]:
+              if child.tag=='parameter':
+                  params = params +child.attrib['name'] + '='+child.text.replace(" ","")  + ","... 
+           print params[:-1]
+
+              > targetwalkers=4000,reconfiguration=no,warmupSteps=50,timestep=0.001,steps=30,blocks=200,nonlocalmoves=yes
+
+
+	'''
+
+
+         ''' schema:
+        
+
+             qmc Tables:
+          
+                 wavefunction: 
+  	                     id_num
+
+                 particleset:
+  	                     id_num
+
+                 optimizationFile:
+  	                     id_num
+                             vmc_method_tags
+			     vmc_estimator
+			     vmc_parameters
+			     opt_loop1_tags
+                             opt_estimator1
+			     opt_costFunc1
+			     opt_parameter1
+			     opt_loop2_tags	
+                             opt_estimator2
+			     opt_costFunc2
+			     opt_parameter2
+
+                 dmc file:
+  	                     id_num
+                             vmc_method_tags
+			     vmc_estimator
+			     vmc_parameters
+                             dmc_method_tags
+			     dmc_estimator
+			     dmc_parameters
+  
+                 connectResults_to_input:
+                              qmc_id_num
+                              energy_location
+
+ 	'''
+        wfsname  = arguments["--wfs_flnm"] 
+        ptclname = arguments["--ptcl_flnm"]
+        optname  = arguments["--opt_flnm"]
+        dmcname  = arguments["--dmc_flnm"]
+
+        from unpackXML import *
+
+        runNum = getNum()
+        pullDataFromWFS(wfsname,runNum)
+        pullDataFromPTCL(ptclname,runNum)
+        pullDataFromOPT(optname,runNum)
+        pullDataFromDMC(dmcname,runNum)
 	
 
         add_qmc_input_metadata()
---
+++ unpackXML.py
@@ -0,0 +1,151 @@
+# -*- coding: utf-8 -*-
+
+#def pullDataFromWFS(filename,runNum):
+#def pullDataFromPTCL(filename,runNum):
+def pullDataFromOPT(filename,runNum):
+        from lxml import etree
+
+	tree= etree.parse(filename)
+	root = tree.getroot()
+
+	id_num=runNum
+	vmc_method_tags = ""
+   	vmc_estimator = "" 
+   	vmc_parameters= "" 
+	opt_loop1_tag = ""
+	opt_loop1_qmc  = ""
+	opt_estimator1 = ""
+	opt_costFunc1 = ""
+	opt_parameter1 = ""
+	opt_loop2_tag = ""
+	opt_loop2_qmc  = ""
+	opt_estimator2 = ""
+        opt_costFunc2  = ""                  
+        opt_parameter2   = ""                    
+
+        for child in root:
+            if child.tag=='qmc':
+		for x in child.attrib:
+		    vmc_method_tags = vmc_method_tags +x+ '='+child.attrib[x]+","	
+		for y in child:
+	  	    if y.tag=="estimator":
+			for x in y.attrib:
+			    vmc_estimator = vmc_estimator + x + "="+ y.attrib[x] + ","
+	 	    else:
+			vmc_parameters = vmc_parameters + y.attrib['name'] + "="+ y.text.replace(" ","") + "," 
+            elif child.tag =="loop":
+		if len(opt_loop1_tag)<1:#This is the first opt loop
+			opt_loop1_tag = "max=" + child.attrib["max"]
+                        for x in child[0].attrib:
+				opt_loop1_qmc = opt_loop1_qmc + x + "=" + child[0].attrib[x]+","
+
+                       	for y in child[0]:
+				if y.tag =="estimator":
+					for x in y.attrib:
+						opt_estimator1 = opt_estimator1 + x+ "=" + y.attrib[x] + ","
+				elif y.tag =="cost":
+					opt_costFunc1 = opt_costFunc1 + y.attrib['name']+ "=" + y.text.replace(" ","")+","
+				else:
+					opt_parameter1 = opt_parameter1 + y.attrib['name']+ "=" + y.text.replace(" ","")+ ","
+            	else: # this is the second opt loop
+			opt_loop2_tag = "max=" + child.attrib["max"]
+                        for x in child[0].attrib:
+				opt_loop2_qmc = opt_loop2_qmc + x + "=" + child[0].attrib[x]+","
+
+			for y in child[0]:
+				if y.tag =="estimator":
+					for x in y.attrib:
+						opt_estimator2 = opt_estimator2 + x+ "=" + y.attrib[x] + ","
+				elif y.tag =="cost":
+					opt_costFunc2 = opt_costFunc2 + y.attrib['name']+ "=" + y.text.replace(" ","")+","
+				else:
+					opt_parameter2 = opt_parameter2 + y.attrib['name']+ "=" + y.text.replace(" ","")+ ","
+
+                                     
+	vmc_method_tags = vmc_method_tags[:-1]
+	vmc_estimator = vmc_estimator[:-1]
+	vmc_parameters= vmc_parameters[:-1]
+	opt_loop1_qmc = opt_loop1_qmc[:-1]
+	opt_estimator1 = opt_estimator1[:-1]
+	opt_costFunc1 = opt_costFunc1[:-1]
+	opt_parameter1 = opt_parameter1[:-1]
+	opt_loop2_qmc = opt_loop2_qmc[:-1]
+	opt_estimator2 = opt_estimator2[:-1]
+	opt_costFunc2 = opt_costFunc2[:-1]
+	opt_parameter2 = opt_parameter2[:-1]
+
+
+'''
+	print "id_num=",runNum
+	print "vmc_method_tags =",vmc_method_tags
+   	print "vmc_estimator=",vmc_estimator
+   	print "vmc_parameters=",vmc_parameters
+	print "opt_loop1_tag =",opt_loop1_tag 
+	print "opt_loop1_qmc=",opt_loop1_qmc
+	print "opt_estimator1=",opt_estimator1
+	print "opt_costFunc1 =",opt_costFunc1 
+	print "opt_parameter1=",opt_parameter1
+	print "opt_loop2_tag =",opt_loop2_tag 
+	print "opt_loop2_qmc =",opt_loop2_qmc 
+	print "opt_estimator2=",opt_estimator2
+        print "opt_costFunc2 =",opt_costFunc2 
+        print "opt_parameter2=",opt_parameter2
+
+'''
+
+
+
+def pullDataFromDMC(filename,runNum):
+        from lxml import etree
+	
+	tree= etree.parse(filename)
+	root = tree.getroot()
+ 	
+  	id_num=runNum
+        vmc_method_tags=""
+   	vmc_estimator = "" 
+   	vmc_parameters= "" 
+        dmc_method_tags=""
+   	dmc_estimator = "" 
+   	dmc_parameters= "" 
+
+
+        for child in root:
+            if child.tag=='qmc':
+                if child.attrib["method"]=="vmc":
+		    for x in child.attrib:
+			vmc_method_tags = vmc_method_tags +x+ '='+child.attrib[x]+","	
+		    for y in child:
+			if y.tag=="estimator":
+				for x in y.attrib:
+				    vmc_estimator = vmc_estimator + x + "="+ y.attrib[x] + ","
+			else:
+				vmc_parameters = vmc_parameters + y.attrib['name'] + "="+ y.text.replace(" ","") + "," 
+
+                elif child.attrib["method"]=="dmc":
+		    for x in child.attrib:
+			dmc_method_tags = dmc_method_tags +x+ '='+child.attrib[x]+","
+
+		    for y in child:
+			if y.tag=="estimator":
+				for x in y.attrib:
+				    dmc_estimator = dmc_estimator + x + "="+ y.attrib[x] + ","
+			else:
+				dmc_parameters = dmc_parameters + y.attrib['name'] + "="+ y.text.replace(" ","") + "," 
+	
+	vmc_method_tags = vmc_method_tags[:-1]
+	vmc_estimator = vmc_estimator[:-1]
+	vmc_parameters= vmc_parameters[:-1]
+	dmc_method_tags = dmc_method_tags[:-1]
+	dmc_estimator = dmc_estimator[:-1]
+	dmc_parameters= dmc_parameters[:-1]
+
+        '''
+	print "vmc_method_tags =",vmc_method_tags
+   	print "vmc_estimator=",vmc_estimator
+   	print "vmc_parameters=",vmc_parameters
+	print "dmc_method_tags =",dmc_method_tags
+   	print "dmc_estimator=",dmc_estimator
+   	print "dmc_parameters=",dmc_parameters
+        '''
+

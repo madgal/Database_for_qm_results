66d58e5e14c9853d0b25d97a9e3d80d25cb5925b
madgal <galbraithm001@gmail.com>
2017-07-14 13:00:01 -0500

    Fixed the filename to be *_1Det.dump

--- misc/a2m_trans.py
+++ misc/a2m_trans.py
@@ -10,7 +10,7 @@ ezfio.set_file(NAME+".ezfio")
 ### convert the ao to mo
 ezfio.set_determinants_n_det_max(1)
 ### Now save the 1 det system for qmcpack
-os.system("qp_run save_for_qmcpack "+NAME+".ezfio/ > "+NAME+"_1.dump")
+os.system("qp_run save_for_qmcpack "+NAME+".ezfio/ > "+NAME+"_1Det.dump")
 os.system("qp_run fci_zmq "+NAME+".ezfio/ > "+NAME+".ao2mo.out")
 ezfio.set_determinants_read_wf(True)
 ezfio.set_determinants_n_det_max("NDET")

6655d5a2722ec02e0d69d4aafd81ae17fe991047
madgal <galbraithm001@gmail.com>
2017-07-14 12:34:12 -0500

    Fixed Error in converter.py file so

--- misc/converter.py
+++ misc/converter.py
@@ -6,8 +6,8 @@ import os
 BINDIR ="/soft/applications/qmcpack/github/build_Intel_real/bin"
 
 FILEPATH = "ABSPATH"
-os.system(BINDIR+"/convert4qmc -QP $FILEPATH/DUMPNAME FLAGS" )
-os.rename("sample.Gaussian-G2.xml","$FILEPATH/FileName.wfs.xml")
-os.rename("sample.Gaussian-G2.ptcl.xml","$FILEPATH/FileName.ptcl.xml")
+os.system(BINDIR+"/convert4qmc -QP "+FILEPATH+"/DUMPNAME FLAGS" )
+os.rename("sample.Gaussian-G2.xml",FILEPATH+"/FileName.wfs.xml")
+os.rename("sample.Gaussian-G2.ptcl.xml",FILEPATH+"/FileName.ptcl.xml")
 
 	
